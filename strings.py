24789d5819617df8a8e0b4a079d2054de2bd0fe1
Dipanshu Kumar <67373922+deepanshuchaudhary007@users.noreply.github.com>
2023-06-17 16:59:01 +0530

    Update strings.py

--- strings.py
+++ strings.py
@@ -1,5 +1,9 @@
-#Question: WAP to Split string every n characters but without splitting a word.
+#Question 1: WAP to Split string every n characters but without splitting a word.
 # Answer:
 from textwrap import wrap
 str = "Python is a high-level, general-purpose programming language. Its design philosophy emphasizes code readability with the use of significant indentation via the off-side rule. Python is dynamically typed and garbage-collected"
 print("\n".join(wrap(str, 15)))
+
+# Question 2: Please find the frequency of each charactor in a given string
+# Answer :
+
